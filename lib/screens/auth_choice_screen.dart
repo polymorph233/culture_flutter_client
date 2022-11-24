20fa7a93b9d311b1b9e1f27428c35256386a4332
nep-nep327 <91326644+nep-nep327@users.noreply.github.com>
2022-11-24 14:01:39 +0100

    Updated cover page

--- lib/screens/auth_choice_screen.dart
+++ lib/screens/auth_choice_screen.dart
@@ -12,34 +12,28 @@ class AuthChoiceScreenState extends State<AuthChoiceScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/images/background_welcome_screen.jpg'),
-              fit: BoxFit.cover),
-        ),
-        child: Container(
+        body: Container(
+          color: Colors.black,
           padding: const EdgeInsets.all(96),
           child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Image(
-              image: AssetImage('assets/images/logo2.png'),
-              width: 200,
+            ClipRRect(
+              borderRadius: BorderRadius.circular(32.0),
+              child: const Image(
+
+                image: AssetImage('assets/AppLogo.png'),
+                width: 200,
+              ),
             ),
-            const Spacer(),
+            Container(
+              padding: EdgeInsets.only(left: 24, right: 24, top: 48, bottom: 48),
+              child: Text("FestivalApp", style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white))),
             ElevatedButton(
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all(Colors.white),
                 minimumSize: MaterialStateProperty.all(const Size(170, 50)),
-                side: MaterialStateProperty.all(
-                  const BorderSide(
-                    color: Colors.amberAccent,
-                    width: 2,
-                  ),
-                ),
+
                 shape: MaterialStateProperty.all(
                   RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(50),
@@ -53,10 +47,9 @@ class AuthChoiceScreenState extends State<AuthChoiceScreen> {
                       fontSize: 30,
                       fontWeight: FontWeight.bold)),
             ),
-            const Spacer(),
           ],
         ),
       ),
-    ));
+    );
   }
 }
